4ed0ea84f22bd6c0d76a8903abb79c8ed0d6ed63
shuolin.peng <shuolin.peng@pjbest.com>
2017-06-22 13:13:02 +0800

    commit

--- data/cv_test.py
+++ data/cv_test.py
@@ -10,6 +10,38 @@ from PIL import Image
 from data import CasiaFile
 
 
+class BinaryFilter(object):
+
+    def __init__(self, inverse=False, otsu=False):
+        self.__inverse = inverse
+        self.__otsu_thresh = otsu
+
+    def filter(self, image):
+        if self.__otsu_thresh:
+            return cv2.threshold(image, 0, 255, cv2.THRESH_OTSU | (
+                cv2.THRESH_BINARY_INV if self.__inverse else cv2.THRESH_BINARY))[1]
+        else:
+            return cv2.threshold(image, np.mean(image), 255, (
+                cv2.THRESH_BINARY_INV if self.__inverse else cv2.THRESH_BINARY))[1]
+
+
+class GaborFilter(object):
+
+    def __init__(self, size):
+        orientations = np.arange(0, 180, 22.5)
+        wavelength = 4 * np.sqrt(2)
+        self.__kernels = [
+            cv2.getGaborKernel((size, size), 1.0, orient, wavelength, 0.5, 0, ktype=cv2.CV_32F)
+            for orient in orientations]
+
+    def filter(self, image):
+        gabor_features = np.zeros_like(image)
+        for kernel in self.__kernels:
+            feature = cv2.filter2D(image, cv2.CV_8UC3, kernel)
+            np.maximum(gabor_features, feature, gabor_features)
+        return gabor_features
+
+
 def gabor_filters():
     filters = []
     ksize = [7, 9, 11, 13, 15, 17]  # gabor尺度，6个
@@ -44,21 +76,23 @@ def proc(image, filters):
 
 
 def binary(image):
-    a = cv2.threshold(img, np.mean(image), 255, cv2.THRESH_TOZERO)
-    b = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-    plt.subplot(1, 3, 1)
+    plt.subplot(3, 1, 1)
     plt.imshow(image, cmap='gray')
-    plt.subplot(1, 3, 2)
-    plt.imshow(a[1], cmap='gray')
-    plt.subplot(1, 3, 3)
-    plt.imshow(b[1], cmap='gray')
+    plt.subplot(3, 2, 3)
+    plt.imshow(BinaryFilter().filter(image), cmap='gray')
+    plt.subplot(3, 2, 4)
+    plt.imshow(BinaryFilter(inverse=True).filter(image), cmap='gray')
+    plt.subplot(3, 2, 5)
+    plt.imshow(BinaryFilter(otsu=True).filter(image), cmap='gray')
+    plt.subplot(3, 2, 6)
+    plt.imshow(BinaryFilter(otsu=True, inverse=True).filter(image), cmap='gray')
     plt.show()
 
 
 for ch, img in CasiaFile('1001-c.gnt'):
     # print(ch)
     # proc(img, gabor_filters())
-    # binary(img)
+    binary(img)
     break
 
 
