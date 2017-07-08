42c0c50b9b334664be3a5bdc0f4307ae3882d750
lubricant <redseed@126.com>
2017-07-09 01:47:41 +0800

    commit

---
+++ data/fmt_file.py
@@ -0,0 +1,167 @@
+
+import matplotlib as plt
+
+from data import *
+
+
+class MnistFile(object):
+
+    '''
+    MNIST 手写数字数据集
+        包含 0-9 的数字图片，每张图片大小 28x28
+        训练集：每个数字约 6000 张
+        测试集：每个数字约 1000 张
+    '''
+
+    def __init__(self, image_filename, label_filename):
+        assert exist_path('mnist/' + image_filename) and \
+               exist_path('mnist/' + label_filename)
+        self.__img_filepath = 'mnist/' + image_filename
+        self.__lab_filepath = 'mnist/' + label_filename
+
+    def __iter__(self):
+        img_file = open(get_path(self.__img_filepath), 'rb')
+        lab_file = open(get_path(self.__lab_filepath), 'rb')
+        img_buf, lab_buf = img_file.read(), lab_file.read()
+
+        def read_img():
+            head = struct.unpack_from('>4I', img_buf, 0)
+            magic_num, img_num, img_row, img_col = head
+
+            offset = struct.calcsize('>4I')
+            pixels = img_num * img_row * img_col
+
+            images = struct.unpack_from('>%dB' % pixels, img_buf, offset)
+            assert len(images) / (img_row * img_col) == img_num
+
+            return np.array(images, dtype=np.uint8).reshape(img_num, img_row, img_col)
+
+        def read_lab():
+            head = struct.unpack_from('>2I', lab_buf, 0)
+            magic_num, lab_num = head
+
+            offset = struct.calcsize('>2I')
+            labels = struct.unpack_from('>%dB' % lab_num, lab_buf, offset)
+
+            return np.array(labels, dtype=np.uint8)
+
+        for ch, img in zip(read_lab(), read_img()):
+            np.subtract(255, img, img)
+            yield str(ch), img
+
+
+class CasiaFile(object):
+
+    '''
+    Casia 手写中文数据集
+        采用 GBK 编码，总共包含 3755 个汉字，每张图片的边长在 50 ~ 150 之间不等
+        训练集：每个汉字约 240 个样本
+        训练集：每个汉字约 60 个样本
+    '''
+
+    def __init__(self, filename):
+        assert exist_path('casia/' + filename)
+        self.__filepath = 'casia/' + filename
+
+    def __iter__(self):
+
+        size = get_size(self.__filepath)
+        file = open(get_path(self.__filepath), 'rb')
+
+        while file.tell() < size:
+            length, = struct.unpack('<I', file.read(4))
+            code = file.read(2)
+            col, row = struct.unpack('<2H', file.read(4))
+            data_len = length - 10
+            data_tag = code.decode('gbk')
+            data_img = np.fromstring(file.read(data_len), np.uint8).reshape((row, col))
+            yield (data_tag, data_img)
+
+
+class TFRecordFile(object):
+
+    '''
+    TFRecord 转换器
+        保存大小对齐后的图片以及汉字对应的索引
+    '''
+
+    def __init__(self, filename, dict_map=None):
+        assert filename
+        self.__dict_map = dict_map
+        self.__filepath = 'record/' + filename
+        self.__tfwriter = None
+
+    def __iter__(self):
+        assert exist_path(self.__filepath)
+        for serialized_example in tf.python_io.tf_record_iterator(get_path(self.__filepath)):
+            example = tf.train.Example()
+            example.ParseFromString(serialized_example)
+
+            rows, cols = example.features.feature['shape'].int64_list.value
+            image, = example.features.feature['image'].bytes_list.value
+            label, = example.features.feature['label'].bytes_list.value
+            yield label.decode(), np.fromstring(image, np.uint8).reshape((rows, cols))
+
+    def write(self, ch, img):
+        assert isinstance(ch, str) and \
+               isinstance(img, np.ndarray)
+
+        dict_map = self.__dict_map
+        assert dict_map and (ch in dict_map) and (
+            isinstance(dict_map[ch], int))
+
+        if not self.__tfwriter:
+            if not exist_path('record'):
+                ensure_path('record')
+            self.__tfwriter = tf.python_io.TFRecordWriter(get_path(self.__filepath))
+
+        writer = self.__tfwriter
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[ch.encode()])),
+            'index': tf.train.Feature(int64_list=tf.train.Int64List(value=[dict_map[ch]])),
+            'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img.tostring()])),
+            'shape': tf.train.Feature(int64_list=tf.train.Int64List(value=[*img.shape]))
+        }))
+
+        writer.write(example.SerializeToString())
+
+    def close(self):
+        if self.__tfwriter:
+            self.__tfwriter.close()
+            self.__tfwriter = None
+
+
+if __name__ == '__main__':
+
+    def mnist_test():
+        cnt = 0
+        ch_map = {}
+        # for ch, img in MnistFile('train-images.idx3-ubyte', 'train-labels.idx1-ubyte'):
+        for ch, img in MnistFile('t10k-images.idx3-ubyte', 't10k-labels.idx1-ubyte'):
+            cnt += 1
+            if ch not in ch_map:
+                ch_map[ch] = 0
+            ch_map[ch] += 1
+            print(ch, img.shape, np.max(img), np.min(img))
+            # plt.imshow(img, cmap='gray')
+            # plt.show()
+        print(cnt)
+        print(str(ch_map))
+
+    def casi_test():
+        cnt = 0
+        ch_map = {}
+        for name in list_file('casia/test'):
+            for ch, img in CasiaFile('test/'+name):
+                cnt += 1
+                if ch not in ch_map:
+                    ch_map[ch] = 0
+                ch_map[ch] += 1
+                print(ch, img.shape)
+                # plt.imshow(img, cmap='gray')
+                # plt.show()
+        print(cnt)
+        print(str(ch_map))
+
+    # mnist_test()
+    # casi_test()
--- data/rectifier.py
+++ data/rectifier.py
@@ -1,16 +1,99 @@
-import os
 
-import tensorflow as tf
+import logging
 
-from data import CasiaFile, TFRecordFile
+from data.fmt_file import *
+
+'''
+使用函数前，按照如下目录结构放置好文件：
+data
+|----casia
+|   |----train：[CASIA 训练集文件 (.gnt)]
+|   |----test： [CASIA 测试集文件 (.gnt)]
+|----mnist：    [MNIST 所有文件] 
+|----record:    [留空]
+'''
 
 
-writer = TFRecordFile('test.tfrecords', TFRecordFile.ch_dict()[1])
-for ch, img in CasiaFile('1001-c.gnt'):
-    writer.write(ch, img)
-writer.close()
+def prepare_label_dict(dict_path='labels_dict.npy'):
+    '''
+    构建 label 字典文件并以 npy 格式保存
+    '''
+
+    def build_label_mapping(casia_files):
+
+        chinese = []
+        for file in casia_files:
+            for cn, _ in file:
+                chinese.append(cn)
+
+        chinese = sorted(set(chinese), key=lambda x: int(x.encode('GBK').hex(), base=16))
+        classes = [str(digit) for digit in range(10)] + chinese
+
+        id_2_ch, ch_2_id = {}, {}
+        for id in range(len(classes)):
+            ch = classes[id]
+            id_2_ch[id] = ch
+            ch_2_id[ch] = id
+
+        return id_2_ch, ch_2_id
+
+    logging.info('Start preparing label dict ......')
+
+    ch_mapping, id_mapping = build_label_mapping(
+        [CasiaFile('test/' + name) for name in list_file('casia/test')])
+
+    np.save(get_path(dict_path), (ch_mapping, id_mapping))
+
+    logging.info('Finish preparing label dict.')
 
 
-for ch, img in TFRecordFile('test.tfrecords'):
-    print(ch, img)
-    break
+def prepare_image_files(file_name, data_set, dict_path='labels_dict.npy', img_filter=lambda _: _):
+    '''
+    将原始图片文件转换为 TFRecord 格式的文件
+    '''
+
+    logging.info('Start preparing image file ......')
+
+    _, id_mapping = np.load(dict_path)
+    tf_writer = TFRecordFile(file_name, id_mapping)
+
+    def flush_mnist_image(file_list, fetch_num=300):
+        for file in file_list:
+            for ch, img in file:
+                if not fetch_num:
+                    return
+                fetch_num -= 1
+                tf_writer.write(ch, img_filter(img))
+
+    def flush_casia_image(file_list):
+        for file in file_list:
+            for ch, img in file:
+                tf_writer.write(ch, img_filter(img))
+
+    assert data_set.upper() in ('TRAINING', 'TEST', 'ALL')
+
+    if data_set.upper() == 'TRAINING':
+        flush_mnist_image([MnistFile('train-images.idx3-ubyte', 'train-labels.idx1-ubyte')])
+        flush_casia_image([CasiaFile('train/' + name) for name in list_file('casia/train')])
+
+    if data_set.upper() == 'TEST':
+        flush_mnist_image([MnistFile('t10k-images.idx3-ubyte', 't10k-labels.idx1-ubyte')])
+        flush_casia_image([CasiaFile('test/' + name) for name in list_file('casia/test')])
+
+    if data_set.upper() == 'ALL':
+        flush_mnist_image([MnistFile('train-images.idx3-ubyte', 'train-labels.idx1-ubyte'),
+                           MnistFile('t10k-images.idx3-ubyte', 't10k-labels.idx1-ubyte')])
+        flush_casia_image([CasiaFile('train/' + name) for name in list_file('casia/train')] +
+                          [CasiaFile('test/' + name) for name in list_file('casia/test')])
+
+    tf_writer.close()
+
+    logging.info('Finish preparing image file.')
+
+
+if __name__ == '__main__':
+    # prepare_label_dict()
+    # prepare_image_files('training_set.tfr', data_set='TRAINING')
+    # prepare_image_files('test_set.tfr', data_set='TEST')
+    prepare_image_files('data_set.tfr', data_set='ALL')
+
