a56fdcfd3c6e0e3a4ea90f6a9d5ef101a748b51e
shuolin.peng <shuolin.peng@pjbest.com>
2017-07-17 17:37:58 +0800

    commit

--- model/layer.py
+++ model/layer.py
@@ -23,8 +23,35 @@ def __attach_attr(obj, **args):
     return obj
 
 
-def __init_value():
-    pass
+def __random_init(shape, mode):
+
+    __assert_shape(shape, 2, 4)
+    __assert_value(mode.lower(), 'gauss', 'xavier', 'caffe', 'msra')
+
+    mode = mode.lower()
+
+    if len(shape) == 2:  # density
+        fan_in, fan_out = shape
+    else:  # convolution
+        k_size = np.prod(shape[:2])
+        fan_in = float(k_size * shape[2])
+        fan_out = float(k_size * shape[3])
+
+    if mode == 'gauss':
+        with tf.name_scope('gauss_init'):
+            return tf.truncated_normal(shape, 0.0, 0.01, tf.float32)
+
+    if mode == 'msra':
+        stddev = 2.0 / fan_in
+        trunc_stddev = np.sqrt(1.3 * stddev)
+        with tf.name_scope('msra_init'):
+            return tf.truncated_normal(shape, 0.0, trunc_stddev, tf.float32)
+
+    n = fan_in if (mode == 'caffe') else ((fan_in + fan_out) / 2.)
+    limit = np.sqrt(3.0 / n)
+
+    with tf.name_scope('%s_init' % mode):
+        return tf.random_uniform(shape, -limit, limit, tf.float32)
 
 
 def data(name, shape, dtype=tf.float32):
@@ -32,7 +59,7 @@ def data(name, shape, dtype=tf.float32):
     __assert_shape(shape, 1, 2, 4)  # [batch, height, width, channel] [batch, num_classes] [batch] = shape
     __assert_value(dtype, tf.float16, tf.float32, tf.float64, tf.int8, tf.int16, tf.int32, tf.int64)
 
-    return __attach_attr(tf.placeholder(dtype, shape, name), layer=name)
+    return __attach_attr(tf.placeholder(dtype, shape, name), layer_name=name)
 
 
 def loss(name):
@@ -46,12 +73,12 @@ def loss(name):
                   tf.nn.sparse_softmax_cross_entropy_with_logits)  # batch_size = labels.shape
 
         cross_entropy = l_func(logits=logits, labels=labels, name=name)
-        return __attach_attr(tf.reduce_mean(cross_entropy), layer=name, cross_entropy=cross_entropy)
+        return __attach_attr(tf.reduce_mean(cross_entropy), layer_name=name, cross_entropy=cross_entropy)
 
     return __
 
 
-def convolution(name, k_shape, stride=1, padding='SAME', dtype=tf.float32, init='xavier'):
+def convolution(name, k_shape, stride=1, padding='SAME', init='xavier'):
     __assert_type(name, str)
     __assert_type(stride, int)
     __assert_value(padding.upper(), 'VALID', 'SAME')
@@ -64,15 +91,12 @@ def convolution(name, k_shape, stride=1, padding='SAME', dtype=tf.float32, init=
 
         with tf.name_scope(name):
             shape = [k_height, k_width, in_channel, out_channel]
-            fan_in, fan_out = np.prod(shape[:-1]), np.prod(k_shape)
-            bound = np.sqrt(6. / (fan_in + fan_out))
-            rand = tf.random_uniform(shape, minval=-bound, maxval=bound, dtype=dtype)
 
-            filt = tf.Variable(rand, name='filt')
+            filt = tf.Variable(__random_init(shape, init), name='filt')
             conv = tf.nn.conv2d(value, filt, [1, stride, stride, 1], padding.upper())
             bias = tf.Variable(tf.zeros(conv.shape[-1]), name='bias')
             relu = tf.nn.relu(tf.nn.bias_add(conv, bias))
-            return __attach_attr(relu, layer=name, weight=filt, bias=bias)
+            return __attach_attr(relu, layer_name=name, weight=filt, bias=bias)
 
     return __
 
@@ -90,7 +114,7 @@ def pooling(name, p_shape, p_type, stride=1, padding='SAME'):
         stride = [1, stride, stride, 1]
         p_func = tf.nn.max_pool if p_type.upper() == 'MAX' else tf.nn.avg_pool
         return lambda value: __attach_attr(
-            p_func(value, p_shape, stride, padding.upper(), name=name), layer=name, weight=None, bias=None)
+            p_func(value, p_shape, stride, padding.upper(), name=name), layer_name=name, weight=None, bias=None)
 
     def __(value):
         v_depth = value.shape.as_list()[-1]
@@ -103,12 +127,12 @@ def pooling(name, p_shape, p_type, stride=1, padding='SAME'):
             pool = pooling('pool', [p_height, p_width], p_type=p_type,
                            stride=stride, padding=padding)(value)
             return __attach_attr(
-                tf.concat([conv, pool], axis=-1, name='depth_concat'), layer=name, weight=conv.weight, bias=conv.bias)
+                tf.concat([conv, pool], axis=-1, name='depth_concat'), layer_name=name, weight=conv.weight, bias=conv.bias)
 
     return __
 
 
-def density(name, neurons, linear=False, dtype=tf.float32):
+def density(name, neurons, linear=False, init='xavier'):
     __assert_type(name, str)
     __assert_type(neurons, int)
 
@@ -117,14 +141,10 @@ def density(name, neurons, linear=False, dtype=tf.float32):
             value = tf.reshape(value, [-1, np.prod(value.shape[1:].as_list())])
 
             shape = value.shape[1:].as_list() + [neurons]
-            fan_in, fan_out = value.shape.as_list()[-1], neurons
-            bound = np.sqrt(6. / (fan_in + fan_out))
-            rand = tf.random_uniform(shape, minval=-bound, maxval=bound, dtype=dtype)
-
-            weight = tf.Variable(rand, name='weight')
+            weight = tf.Variable(__random_init(shape, init), name='weight')
             bias = tf.Variable(tf.zeros([neurons]), name='bias')
             fc = tf.nn.bias_add(tf.matmul(value, weight), bias)
-            return __attach_attr(fc if linear else tf.nn.relu(fc), layer=name, weight=weight, bias=bias)
+            return __attach_attr(fc if linear else tf.nn.relu(fc), layer_name=name, weight=weight, bias=bias)
 
     return __
 
@@ -137,32 +157,27 @@ def dropout(name, **args):
     with tf.name_scope(name):
         keep_prob = tf.placeholder(tf.float32, name='keep_prob')
         return lambda value: __attach_attr(
-            tf.nn.dropout(value, name='dropout', keep_prob=keep_prob, **args), layer=name, keep_prob=keep_prob)
+            tf.nn.dropout(value, name='dropout', keep_prob=keep_prob, **args), layer_name=name, keep_prob=keep_prob)
 
 
-def inception(name, *graph, dtype=tf.float32):
+def inception(name, *graph):
     assert graph and \
            all(isinstance(x, list) for x in graph) and \
            all(all(isinstance(x, tuple) and len(x) in (2, 3) for x in pipeline) for pipeline in graph)
 
     node_factory = {
         'conv_1x1':
-            lambda init, depth, value: convolution('conv_1x1', [1, 1, depth],
-                                                   padding='SAME', dtype=dtype, init=init)(value),
+            lambda init, depth, value: convolution('conv_1x1', [1, 1, depth], padding='SAME', init=init)(value),
         'conv_3x3':
-            lambda init, depth, value: convolution('conv_3x3', [3, 3, depth],
-                                                   padding='SAME', dtype=dtype, init=init)(value),
+            lambda init, depth, value: convolution('conv_3x3', [3, 3, depth], padding='SAME', init=init)(value),
         'conv_5x5':
-            lambda init, depth, value: convolution('conv_5x5', [5, 5, depth],
-                                                   padding='SAME', dtype=dtype, init=init)(value),
+            lambda init, depth, value: convolution('conv_5x5', [5, 5, depth], padding='SAME', init=init)(value),
         'conv_1x7':
-            lambda init, depth, value: convolution('conv_1x7', [1, 7, depth],
-                                                   padding='SAME', dtype=dtype, init=init)(value),
+            lambda init, depth, value: convolution('conv_1x7', [1, 7, depth], padding='SAME', init=init)(value),
         'conv_7x1':
-            lambda init, depth, value: convolution('conv_7x1', [7, 1, depth],
-                                                   padding='SAME', dtype=dtype, init=init)(value),
+            lambda init, depth, value: convolution('conv_7x1', [7, 1, depth], padding='SAME', init=init)(value),
         'pool_3x3':
-            lambda init, depth, value: convolution('pool_proj', [1, 1, depth], padding='SAME', dtype=dtype, init=init)(
+            lambda init, depth, value: convolution('pool_proj', [1, 1, depth], padding='SAME', init=init)(
                                            pooling('pool_3x3', [3, 3], 'MAX')(value)),
     }
 
@@ -176,7 +191,6 @@ def inception(name, *graph, dtype=tf.float32):
                 for x in pipeline:
                     node_type, node_depth = x[0:2]
                     node_init = 'xavier' if len(x) <= 2 else x[2]
-                    assert node_init in ('xavier', 'gauss')
                     assert node_type in node_factory
                     node = node_factory[node_type](node_init, node_depth, node)
                     path.append((node_type, node))
@@ -184,6 +198,6 @@ def inception(name, *graph, dtype=tf.float32):
                 node_path.append(path)
 
             return __attach_attr(tf.concat(node_stack, axis=-1, name='depth_concat'),
-                                 layer=name, branch_graph=tuple(node_path))
+                                 layer_name=name, branch_graph=tuple(node_path))
 
     return __
